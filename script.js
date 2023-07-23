8b6d839031cf8be215129f0b6f889bd3762bbd20
George Lynch <osu4life75@gmail.com>
2023-07-22 23:59:57 -0500

    Game
    Game design cont.

--- script.js
+++ script.js
@@ -1,6 +1,52 @@
 const cardArray = [
     {
         name: "fries",
-        img: 'images/fries.png',
-    }
+        Image: 'images/fries.png',
+    }, 
+    {
+        name: "hotdog",
+        Image: "images/hotdog.png",
+    },
+    {
+        name: "cheeseburger",
+        Image: "images/cheeseburger.png",
+    },
+    {
+        name: "ice-cream",
+        Image: "images/icecream.png",
+    },
+    {
+        name: "pizza",
+        Image: "images/pizza.png",
+    },
+    {
+        name: "white",
+        Image: "images/white.png",
+    },
+    {
+        name: "blank",
+        Image: "images/blank
+        .png",
+    },
+    {
+        name: "hotdog",
+        Image: "images/hotdog.png",
+    },
+    {
+        name: "hotdog",
+        Image: "images/hotdog.png",
+    },
+    {
+        name: "hotdog",
+        Image: "images/hotdog.png",
+    },
+    {
+        name: "hotdog",
+        Image: "images/hotdog.png",
+    },
+    {
+        name: "hotdog",
+        Image: "images/hotdog.png",
+    },
+
 ]
